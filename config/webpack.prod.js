c3870a114a44d86184fa57737ece396d610439ab
张浩 <halzhan@zhanghaodeMacBook-Pro.local>
2017-01-11 20:27:43 +0800

    dist

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -12,7 +12,7 @@ module.exports = webpackMerge(commonConfig, {
 
   output: {
     path: helpers.root('dist'),
-    publicPath: '/qimager/dist',
+    publicPath: './dist',
     filename: '[name].js',
     chunkFilename: '[id].chunk.js'
   },
